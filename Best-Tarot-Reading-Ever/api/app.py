8ac97b6bbee222b0efe5d444a5a32d4da9acba2c
welovezaf <109696907+welovezaf@users.noreply.github.com>
2023-06-17 23:46:12 +0000

    Pending changes exported from your codespace

--- Best-Tarot-Reading-Ever/api/app.py
+++ Best-Tarot-Reading-Ever/api/app.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
